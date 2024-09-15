516357583a045f2eee803ea84b7e38a57ad92e6c
Morteza Abdolahi <158506777+morteza2323@users.noreply.github.com>
2024-09-15 12:40:24 +0330

    Update func1.js and deleting some features

--- func1.js
+++ func1.js
@@ -1,12 +1,5 @@
 const logger = () => console.log('logger');
 const func2 = () =>
 {
-    
+    logger();
 }
-const func3 = () =>
-{
-    
-}
-console.log('fix bug');
-const func4 = () => console.log('learning git and github');
-alert('sucess');
